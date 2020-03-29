2fa38e2e1b07ab1a9d067be1641aea6e0863f92a
Nikolay Pashov <nikifaets111@gmail.com>
2020-03-29 18:28:22 +0300

    sdf

---
+++ Homework1/Assets/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 offset = new Vector3(0,5,0);
+
+    void Start()
+    {
+        ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("Player");
+        //Debug.Log(player);
+        transform.rotation = Quaternion.Slerp( transform.rotation, Quaternion.LookRotation( player.transform.position - transform.position ), Time.deltaTime*10 );
+
+        Vector3 playerForward = player.transform.forward;
+        playerForward = Vector3.Normalize(playerForward);
+
+        transform.position = player.transform.position - playerForward*5 + offset;
+    }
+}
---
+++ Homework1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public float speed = 3f;
+	private float scaleRotation = 3f;
+	private Vector3 moveDirection = Vector3.zero;
+	private Vector3 rotateDirection = Vector3.zero;
+	CharacterController controller;
+	private float smooth = 0.1f;
+	private float rotateY = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    	
+
+        moveDirection = Input.GetAxis("Vertical") * transform.forward;
+        moveDirection *= speed*Time.deltaTime;
+        //controller.Move(moveDirection*Time.deltaTime);   
+        
+        Debug.Log(moveDirection);
+        transform.position += moveDirection;
+
+        rotateY = Input.GetAxis("Horizontal") * scaleRotation;
+        //Debug.Log(rotateY * scaleRotation);
+        Debug.Log(scaleRotation);
+       	transform.Rotate(0, rotateY, 0);
+
+    }
+}
